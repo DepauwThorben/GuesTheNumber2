81db7c937f734b8bec220373def01e2b38d8b8c8
Thorben Depauw <depauwthorben@gmail.com>
2020-03-14 14:10:41 +0100

    commit 14 03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CounterService } from './counter.service';
 import {MaterialModule} from './material.module';
-import {FlexLayoutModule} from '@angular/flex-layout'
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MaterialModule, FlexLayoutModule ],
+  imports:      [ BrowserModule, FormsModule, MaterialModule, FlexLayoutModule, BrowserAnimationsModule ],
   providers:    [ CounterService ],
   declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
