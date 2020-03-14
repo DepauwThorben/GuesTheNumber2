e2553e676786e2cb3bd4271027a67495afc1f8f6
Thorben Depauw <depauwthorben@gmail.com>
2020-03-14 14:12:09 +0100

    commit 14 03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CounterService } from './counter.service';
-import {MaterialModule} from './material.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MaterialModule, FlexLayoutModule, BrowserAnimationsModule ],
+  imports:      [ BrowserModule, FormsModule, MaterialModule, FlexLayoutModule ],
   providers:    [ CounterService ],
   declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
-    imports: [MatButtonModule,],
-    exports: [MatButtonModule,]
+    imports: [MatButtonModule],
+    exports: [MatButtonModule]
 })
 
 export class MaterialModule {}
